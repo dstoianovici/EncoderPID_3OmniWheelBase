6e74ea8e2f204524f9f04bef202baa824a2dbbd7
Dan Stoianovici <stoianovici.dan.s@gmail.com>
2020-02-10 10:45:50 -0700

    first commit

---
+++ lib/Serial_Parser/src/Serial_Parser.h
@@ -0,0 +1,22 @@
+#ifndef SERIAL_PARSER_H
+#define SERIAL_PARSER_H
+
+#include <Arduino.h>
+
+class Serial_Parser
+{
+  public:
+    Serial_Parser(char delimiter, int range_M, int range_m);
+    // int GetNumParams();
+    int* GetParams(int* nums);
+
+
+  private:
+    // long _baud_rate;
+    char _delimiter;
+    int _range_M;
+    int _range_m;
+
+};
+
+#endif
